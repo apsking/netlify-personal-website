ad804cc312ed554876d52be1d8d7e5bfc2e4ec31
Alex King <>
2023-03-02 19:12:01 -0500

    Add projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,28 +6,24 @@ export default function Home() {
     <Layout>
       <Header title="Hi, my name is Alex." />
         <p>
-          I love writing beautiful code and using technology to make our lives easier, more productive, and more enjoyable.
-        </p>
-        <p>
-          I'm a life-long student, a software engineer by trade, and a
-          tinkerer at heart. I have a full-stack background and have worked
-          at places like Microsoft and Amazon.
-          Check out my <a href="https://www.linkedin.com/in/apsking/">LinkedIn</a> for
+          I'm a life-long student, a software engineer by trade, and a tinkerer at heart. I have a full-stack background 
+          and have worked at places like Microsoft and Amazon. Check out
+          my <a href="https://www.linkedin.com/in/apsking/">LinkedIn</a> or <a href='https://github.com/apsking'>GitHub</a> for
           a little more about me.
         </p>
         <p>
-          Most recently, I have been interested in building voice applications
-          on AWS and Amazon Alexa. I was one of the first developers to integrate
-          <a href="https://www.amazon.com/Alexander-King-My-Pet-Rock/dp/B073VDPTCW"> Alexa's
-          in-skill purchasing</a> and really love how that opens the market
-          for voice applications. I've tinkered with things like the smarthome automation
-          using AWS IoT, keeping up with the latest JS frameworks, trying to
-          harness the AWS CDK to maximize its potential, and much more.
+          Most recently, I have been interested in building voice applications on AWS and Amazon Alexa. I was one of the first
+          developers to integrate <a href="https://www.amazon.com/Alexander-King-My-Pet-Rock/dp/B073VDPTCW"> Alexa's in-skill
+          purchasing</a> and really love how that opens the market for voice applications. I've tinkered with things like the
+          smarthome automation using AWS IoT, keeping up with the latest JS frameworks, trying to harness the AWS CDK to
+          maximize its potential, and much more.
+        </p>
+        <p>
+          I love writing beautiful code and using technology to make our lives easier, more productive, and more enjoyable.
         </p>
         <p>
           Check out my projects <a href='/projects'>here.</a>
         </p>
-        
     </Layout>
   )
 }
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,13 +1,87 @@
-import Header from '../../components/Header'
+import Header from '../../components/Header';
 import Layout from '../../components/Layout';
+import { ReactNode } from 'react';
+
+type Project = {
+  title: string,
+  body: ReactNode
+}
+
+const projects: Project[] = [
+  {
+    title: '🦍 Gorilla 🦍',
+    body: (
+      <>
+        <p>
+          <b>Stop monkeying around and write better scripts</b>
+        </p>
+        <p>
+        <a href='https://github.com/apsking/gorilla'>Gorilla</a> is a blazing fast, TypeScript build tool for creating better
+        GreaseMonkey scripts. It handles the complex build chain, so you don't have to. Find it
+        on <a href='https://www.npmjs.com/package/gorilla-build'>NPM</a> as <b>gorilla-build</b>.
+        </p>
+      </>
+    ),
+  },
+  {
+    title: 'Alexa Skill - My Pet Rock 🪨',
+    body: (
+      <>
+        <p>
+          In <a href="https://www.amazon.com/Alexander-King-My-Pet-Rock/dp/B073VDPTCW" target="_blank" rel="noopener noreferrer">My Pet
+          Rock</a>, you get to take care of your own pet. Take your new pet on walks, play games with it, and feed it regularly,
+          so that it grows big and strong! Your pet will grow up over time to learn more skills and tricks. Join nearly than 500,000
+          other people and see what it's all about!
+        </p>
+      </>
+    ),
+  },
+  {
+    title: 'Alexa Skill - Interview Prep 📈',
+    body: (
+      <>
+        <p>
+			    <a href="https://www.amazon.com/Alexander-King-Interview-Prep/dp/B01LW7L2VD" target="_blank"rel="noopener noreferrer">Interview
+          Prep</a> is your one-stop skill to help you prepare for your next interview! It has hundreds of questions for a variety of
+          different job types and areas of work. For best results, ask Interview Prep a few questions every day to prepare your answers.
+          Consider answering directly into a mirror and focusing not only on your answer, but also your body language!
+		    </p>
+        <p>
+          This skill was featured on <a href="https://lifehacker.com/how-to-use-alexa-to-help-you-prep-for-a-job-interview-1833024806">LifeHacker!</a>
+        </p>
+      </>
+    ),
+  },
+  {
+    title: 'Alexa Skill - Ancient Alchemy 🪄',
+    body: (
+      <p>
+			  In <a href="https://www.amazon.com/Alexander-King-Ancient-Alchemy/dp/B074F4NHJ8" target="_blank"rel="noopener noreferrer">Ancient Alchemy</a>,
+        you take on the challenge of restoring the Library of Elements by finding recipes for all of the elements in the realm. As The Alchemist,
+        you start by combining primitive elements and then continue to use your findings to make many more! This creative journey will immerse you
+        in its world and challenge your abilities!
+		  </p>
+    )
+  }
+];
 
 export default function Projects() {
+  const projectList = projects.map(({ title, body}, i) => (
+    <div key={i}>
+      <h2>{title}</h2>
+      {body}
+    </div>
+  ));
+
   return (
     <Layout>
       <Header title="Projects" />
-        <p>
-          More about my projects here.
-        </p>
+      <p>
+        You can find most of my projects <a href='https://github.com/apsking'>on GitHub.</a>
+      </p>
+      <div>
+        { projectList }
+      </div>
     </Layout>
   )
 }
